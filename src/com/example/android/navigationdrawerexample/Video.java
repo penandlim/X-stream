dcc35f8f4bbb56166784508aa7672ccfd823d802
starywars <starywars@users.noreply.github.com>
2014-06-30 00:30:02 -0400

    I'm dumb

--- src/com/example/android/navigationdrawerexample/Video.java
+++ src/com/example/android/navigationdrawerexample/Video.java
@@ -78,7 +78,7 @@ public class Video {
         if (dur.isEmpty()) dur = "allduration";
         search = search.replace(" ", "+");
         String base = "http://www.xvideos.com/?k=";
-        current_url = base + search + "&sort=" + sort + "&durf" + dur + "&datef=" + date;
+        current_url = base + search + "&sort=" + sort + "&durf=" + dur + "&datef=" + date;
         return current_url;
     }
 
@@ -134,7 +134,7 @@ public class Video {
         if (dur.isEmpty()) dur = "allduration";
         String base = "http://www.xnxx.com/?k=";
         search = search.replace(" ", "+");
-        current_url = base + search + "&sort=" + sort + "&durf" + dur + "&datef=" + date;
+        current_url = base + search + "&sort=" + sort + "&durf=" + dur + "&datef=" + date;
         return current_url;
     }
 
